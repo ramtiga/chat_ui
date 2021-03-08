12e129acbf909e9ac061031664cfeae7b38bc0b3
dhane31 <dhanegm731@gmail.com>
2021-03-08 16:57:12 +0900

    Add favorite

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -11,24 +11,20 @@ class Message {
 }
 
 final User currentUser =
-    User(id: 0, name: 'Current User', imageUrl: 'assets/images/user0.jpg');
+    User(id: 0, name: 'Current User', imageUrl: 'assets/images/user0.png');
 
-final User user1 =
-    User(id: 1, name: 'User1', imageUrl: 'assets/images/user1.jpg');
+final User user1 = User(id: 1, name: 'まさ', imageUrl: 'assets/images/user1.png');
 final User user2 =
-    User(id: 2, name: 'User2', imageUrl: 'assets/images/user2.jpg');
+    User(id: 2, name: 'ゆうか', imageUrl: 'assets/images/user2.png');
 final User user3 =
-    User(id: 3, name: 'User3', imageUrl: 'assets/images/user3.jpg');
+    User(id: 3, name: 'たろう', imageUrl: 'assets/images/user3.png');
 final User user4 =
-    User(id: 4, name: 'User4', imageUrl: 'assets/images/user4.jpg');
-final User user5 =
-    User(id: 5, name: 'User5', imageUrl: 'assets/images/user5.jpg');
-final User user6 =
-    User(id: 6, name: 'User6', imageUrl: 'assets/images/user6.jpg');
-final User user7 =
-    User(id: 7, name: 'User7', imageUrl: 'assets/images/user7.jpg');
+    User(id: 4, name: 'いちろう', imageUrl: 'assets/images/user4.png');
+final User user5 = User(id: 5, name: 'ひろ', imageUrl: 'assets/images/user5.png');
+final User user6 = User(id: 6, name: 'さく', imageUrl: 'assets/images/user6.png');
+final User user7 = User(id: 7, name: 'まい', imageUrl: 'assets/images/user7.png');
 
-List<User> favorites = [user2, user5, user1, user7];
+List<User> favorites = [user2, user5, user1, user4, user6, user7];
 
 List<Message> chats = [
   Message(
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'package:chat_ui/models/message.dart';
+import 'package:chat_ui/pages/chat.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -48,6 +50,68 @@ class HomePage extends HookWidget {
                   ),
                 );
               },
+            ),
+          ),
+          Container(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Favorite contacts",
+                            style: TextStyle(
+                              color: Colors.blueGrey,
+                              fontSize: 18.0,
+                            )),
+                        IconButton(
+                            icon: Icon(Icons.more_horiz), onPressed: () {})
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 120.0,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: favorites.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return GestureDetector(
+                            onTap: () => Navigator.push(context,
+                                MaterialPageRoute(builder: (_) => ChatPage())),
+                            child: Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 6.0),
+                                  Container(
+                                    width: 60.0,
+                                    height: 60.0,
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        image: DecorationImage(
+                                            fit: BoxFit.fill,
+                                            image: AssetImage(
+                                                favorites[index].imageUrl))),
+                                  ),
+                                  Text(
+                                    favorites[index].name,
+                                    style: TextStyle(
+                                      color: Colors.blueGrey,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
